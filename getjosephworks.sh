30a498a9cb72d04baa7b629bb1843f4ff5697281
Joseph Robinson <smartech56@gmail.com>
2018-07-19 22:49:24 -0700

    Add files via upload

---
+++ getjosephworks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Pre-Init
+toilet Starting...
+toilet josephworks
+echo installing depens
+sudo apt install git toilet
+mkdir josephworks
+cd josephworks
+
+# Main
+echo "Cloning all Repositories to ./josephworks"
+git clone https://github.com/josephworks/files
+git clone https://github.com/josephworks/McEmeraldtnt
+git clone https://github.com/josephworks/McAdventure
+git clone https://github.com/josephworks/ultimatelinux.sh
+git clone https://github.com/josephworks/josephworks.github.io
+git clone https://github.com/josephworks/MinecraftMultiversion
+git clone https://github.com/josephworks/McCompile
+git clone https://github.com/josephworks/Gwen-Remade
+git clone https://github.com/josephworks/CMDWorkspaceTools
+git clone https://github.com/josephworks/serverbuilder
+git clone https://github.com/josephworks/WebIDE
+git clone https://github.com/josephworks/linux-update
+git clone https://github.com/josephworks/JenkinsServer
+git clone https://github.com/josephworks/Registry-Edits
+git clone https://github.com/josephworks/PaperCraft
+git clone https://github.com/josephworks/SpigotCraft
+git clone https://github.com/josephworks/ThermosServer
+git clone https://github.com/josephworks/Table-Viewer
+git clone https://github.com/josephworks/Gideon
+git clone https://github.com/josephworks/thunderstorm
+git clone https://github.com/josephworks/Windows-subsystem-for-Linux
+git clone https://github.com/josephworks/Google-Chrome-Linux-root-fix
+git clone https://github.com/josephworks/PaperTestServer
+git clone https://github.com/josephworks/Powershell-policy-fix
+echo "Finished the cloning process"
+toilet Done
