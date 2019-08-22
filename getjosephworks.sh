1f03d208e64b880b93fe6ef279a98bebee265d97
Joseph Robinson <smartech56@gmail.com>
2019-08-22 15:22:38 -0700

    Update getjosephworks.sh

--- getjosephworks.sh
+++ getjosephworks.sh
@@ -9,19 +9,28 @@ mkdir josephworks
 cd josephworks
 
 # Main
-echo "Cloning all Repositories to ./josephworks"
+echo "Cloning all Repositories to ./josephworks ..."
+git clone https://github.com/josephworks/about
 git clone https://github.com/josephworks/AtomMC
+git clone https://github.com/josephworks/csharp
+git clone https://github.com/josephworks/Kettle
 git clone https://github.com/josephworks/HelpDesk
 git clone https://github.com/josephworks/BungeeSpigot
+git clone https://github.com/josephworks/BookManager
 git clone https://github.com/josephworks/Scripts
 git clone https://github.com/josephworks/startxcli.sh
 git clone https://github.com/josephworks/files
+git clone https://github.com/josephworks/vbnet
+git clone https://github.com/josephworks/Volcano
 git clone https://github.com/josephworks/McEmeraldtnt
 git clone https://github.com/josephworks/McAdventure
 gti clone https://github.com/josephworks/MCPluginDB
 git clone https://github.com/josephworks/ultimatelinux.sh
 git clone https://github.com/josephworks/josephworks
 git clone https://github.com/josephworks/josephworks.github.io
+git clone https://github.com/josephworks/josephworks.ddns.net
+git clone https://github.com/josephworks/matrixdevteam.ml
+git clone https://github.com/josephworks/games
 git clone https://github.com/josephworks/MinecraftMultiversion
 git clone https://github.com/josephworks/McCompile
 git clone https://github.com/josephworks/Gwen-Remade
@@ -31,6 +40,9 @@ git clone https://github.com/josephworks/WebIDE
 git clone https://github.com/josephworks/JenkinsServer
 git clone https://github.com/josephworks/Registry-Edits
 git clone https://github.com/josephworks/PaperCraft
+git clone https://github.com/josephworks/Paper
+git clone https://github.com/josephworks/Paint
+git clone https://github.com/josephworks/Glowstone
 git clone https://github.com/josephworks/pythonws
 git clone https://github.com/josephworks/SpigotCraft
 git clone https://github.com/josephworks/ThermosServer
@@ -38,4 +50,7 @@ git clone https://github.com/josephworks/Gideon
 git clone https://github.com/josephworks/Google-Chrome-Linux-root-fix
 git clone https://github.com/josephworks/Powershell-policy-fix
 echo "Finished the cloning process"
+echo "Creating Update Script..."
+echo "find . -type d -mindepth 1 -maxdepth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;" > update.sh
+echo "Created Update Script"
 toilet Done
